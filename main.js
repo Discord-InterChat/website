59ff2c4c0746b1d83716741b9d3edc35f82c6ba4
dev-737 <73829355+dev-737@users.noreply.github.com>
2022-05-09 12:02:10 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const doc = document;
+const menuOpen = doc.querySelector(".menu");
+const menuClose = doc.querySelector(".close");
+const overlay = doc.querySelector(".overlay");
+
+menuOpen.addEventListener("click", () => {
+  overlay.classList.add("overlay--active");
+});
+
+menuClose.addEventListener("click", () => {
+  overlay.classList.remove("overlay--active");
+});
+
+let progress = document.getElementById("progressbar");
+let totalHeight = document.body.scrollHeight - window.innerHeight;
+window.onscroll = function () {
+  let progressHeight = (window.pageYOffset / totalHeight) * 100;
+  //progress.style.height = progressHeight + "%";
+  progress.style.width = progressHeight + "%";
+};
